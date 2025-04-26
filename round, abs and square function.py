34673ad68e2ea1da7499594bc7f0f714149936d1
Biplab1992 <neubiplab@gmail.com>
2025-04-26 12:45:25 +1200

    aa

--- round, abs and square function.py
+++ round, abs and square function.py
@@ -1,15 +1,22 @@
-import math
 
-length = float(input("Enter the length of the rectangle:"))
-width = float(input("Enter the width of the rectangle:"))
 
-length = abs(length)
-width = abs(width)
+#length = float(input("Enter the length of the rectangle:"))
+#width = float(input("Enter the width of the rectangle:"))
+
+length,width = input("Input value (length & width):").split()
+
+length = int(length)
+width = int(width)
 
 #avg_side = (length+width)
-#square_area = math.pow(avg_side,2)#
+#square_area = math.pow(avg_side,2)
 
-area = length*width
-rounded_area = math.pow(area,2)
-print(f"Area", area)
-print("square", rounded_area)
+# area = 
+# # rounded_area = math.pow(area,2)
+# print(f"Area", area)
+# print("square", rounded_area)
+
+def rectangle (length,width):
+    area_rectangle= length*width
+    return (area_rectangle)
+rectangle(length,width)
